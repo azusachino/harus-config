426bfc9f12a388313fd59999faf90688cb4bdbb3
haru <azusa146@gmail.com>
2025-03-24 15:38:44 +0900

    feat. dotenv with fish

--- config.fish
+++ config.fish
@@ -16,9 +16,7 @@ else if test -d /opt/homebrew # MacOS
 	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
 end
 
-fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
-! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
-! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
 
 ## alias
 # shortcuts & alias
@@ -26,4 +24,38 @@ alias mci="mvn clean install -Dmaven.test.skip=true"
 alias mcd="mvn clean deploy -DskipTests"
 
 alias kl="kubectl logs -f --tail 200 "
-alias ss="ssh -o StrictHostKeyChecking=no"
+alias ss="ssh -o StrictHostKeyChecking=no "
+
+alias sk="sudo k3s kubectl "
+
+## functions
+#
+function dotenv
+    for line in (cat $argv[1])
+        set line (string trim $line)
+        if test -z $line
+        or string match -q "#*" $line
+            continue
+        end
+        set name_value (string split -m 1 = $line)
+        set name (string trim $name_value[1])
+        set value (string trim $name_value[2])
+        #echo "[line=$line,name_value=$name_value,name=$name,value=$value]"
+        if string match -q '"*"' $value
+        or string match -q "'*'" $value
+            set value (string sub -s 2 -e -1 $value)
+        end
+        if string match -qr '[$][{][A-Za-z_][A-Za-z_0-9]*[}]' $sub1
+            set sub1 (string replace '[$][{]([A-Za-z_][A-Za-z_0-9]*)[}]' '$$$1' $sub1)
+        else
+            set sub1 $value
+        end
+        if string match -qr '[$][A-Za-z_][A-Za-z_0-9]*' $sub1
+            set sub2 (eval "echo $value")
+        else
+            set sub2 $value
+        end
+        #echo "[name=$name,value=$value,sub1=$sub1,sub2=$sub2]"
+        set -gx $name $sub2
+    end
+end
